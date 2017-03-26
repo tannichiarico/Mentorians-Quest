99fd40f6724c0be73e4858503b842b954a0e38a9
tannichiarico <tannichiarico@ruelala.com>
2017-03-26 12:26:08 -0400

    Cmd line input
    trying to figure out how this works

---
+++ src/input.py
@@ -0,0 +1,18 @@
+#!python
+
+
+
+def main():
+	while True:
+		teamName = raw_input("What is your team's name?>> ")
+		print "Hello %s!" %teamName
+
+		setupStatus = raw_input("Are you ready to begin, y/n?>> ")
+		if setupStatus = "y"
+			break
+		
+	print teamName
+ 
+ main()
+
+ pause
---
+++ src/run.bat
@@ -0,0 +1,3 @@
+@echo off
+python input.py
+pause
